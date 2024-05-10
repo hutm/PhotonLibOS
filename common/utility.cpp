ef6901b1071ceda4a8f5b7e6431654926190bd75
Hut <hadkevich@gmail.com>
2024-05-10 09:32:51 -0700

    musl compatibility

--- common/utility.cpp
+++ common/utility.cpp
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 #include <sys/utsname.h>
-#include <execinfo.h>
+//#include <execinfo.h>
 #include "utility.h"
 #include "estring.h"
 #include "alog.h"
@@ -59,14 +59,14 @@ int kernel_version_compare(std::string_view dst, int& result) {
 }
 
 void print_stacktrace() {
-    int size = 16;
-    void * array[16];
-    int stack_num = backtrace(array, size);
-    char ** stacktrace = backtrace_symbols(array, stack_num);
-    for (int i = 0; i < stack_num; ++i)
-    {
-        LOG_DEBUG(stacktrace[i]);
-    }
-    free(stacktrace);
+//    int size = 16;
+//    void * array[16];
+//    int stack_num = backtrace(array, size);
+//    char ** stacktrace = backtrace_symbols(array, stack_num);
+//    for (int i = 0; i < stack_num; ++i)
+//    {
+//        LOG_DEBUG(stacktrace[i]);
+//    }
+//    free(stacktrace);
 }
 
